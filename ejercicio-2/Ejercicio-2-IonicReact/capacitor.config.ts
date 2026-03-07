accd7cfadb0b5ce9571aba788be6f708aa2615dc
hellencc-dev <hellen124cuenu@gmail.com>
2026-03-07 14:51:43 -0500

    Create del Parcial 1

---
+++ ejercicio-2/Ejercicio-2-IonicReact/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ejercicio-2-IonicReact',
+  webDir: 'dist'
+};
+
+export default config;
